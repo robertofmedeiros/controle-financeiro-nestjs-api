c0c5d5443b6fd17f3e49c898f1b7b6a57be1c793
robertofmedeiros <40504677+robertofmedeiros@users.noreply.github.com>
2026-06-25 21:03:32 -0300

    Bind app to all network interfaces on startup

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Example methods
     },
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
