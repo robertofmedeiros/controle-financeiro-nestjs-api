82ca2f108606e15bb2b1447b776860ec961d9f66
robertofmedeiros <40504677+robertofmedeiros@users.noreply.github.com>
2026-06-27 00:22:48 -0300

    Modify CORS settings for app
    Updated CORS configuration to allow credentials and changed methods to an array format.

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://192.168.10.9:3000'], // ou ['http://192.168.10.9:3000']
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: false,
+    origin: ['http://192.168.10.9:3000'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true,
   });
   
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
