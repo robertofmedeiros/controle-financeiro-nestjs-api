45edf65e7ab3fc79bcb9a960e7814c2b3ae47742
robertofmedeiros <40504677+robertofmedeiros@users.noreply.github.com>
2026-06-27 00:15:08 -0300

    Add OPTIONS method to CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
       origin: '*', // Example origin
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Example methods
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Example methods
     },
   });
 
