f80ad80c662fb8070e1b32792191a3bda8575938
robertofmedeiros <40504677+robertofmedeiros@users.noreply.github.com>
2026-06-27 00:18:06 -0300

    Refactor CORS settings in main.ts
    Updated CORS configuration to allow all origins and specify methods.

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: '*', // Example origin
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Example methods
-    },
-  });
+  const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: true, // ou ['http://192.168.10.9:3000']
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: false,
+  });
   
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
