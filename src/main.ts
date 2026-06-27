670a79430e473adae246db1a27898b0470adc12a
robertofmedeiros <40504677+robertofmedeiros@users.noreply.github.com>
2026-06-27 00:19:57 -0300

    Update CORS origin configuration

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true, // ou ['http://192.168.10.9:3000']
+    origin: ['http://192.168.10.9:3000'], // ou ['http://192.168.10.9:3000']
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: false,
   });
