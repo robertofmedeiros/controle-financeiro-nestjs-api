7794c91eb7578ac88767f427991f206df6c72d9f
robertofmedeiros <40504677+robertofmedeiros@users.noreply.github.com>
2026-06-27 00:30:31 -0300

    Add logging for Access-Control-Allow-Origin header
    Log the Access-Control-Allow-Origin header after response finish.

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,17 @@ async function bootstrap() {
     next();
   });
 
+  app.use((req, res, next) => {
+    res.on('finish', () => {
+      console.log(
+        'Access-Control-Allow-Origin:',
+        res.getHeader('Access-Control-Allow-Origin'),
+      );
+    });
+  
+    next();
+  });
+
   app.enableCors({
     origin: ['http://192.168.10.9:3000'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
